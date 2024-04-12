7f1027f9fa49407f2d181d1e8516237ed997a705
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-04-12 16:45:30 +0200

    creazione tabella treni e migrazione

---
+++ database/migrations/2024_04_12_142729_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('company');
+            $table->string('departure');
+            $table->string('arrival');
+            $table->string('company');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code');
+            $table->tinyInteger('carriage_number');
+            $table->boolean('is_it_on_time');
+            $table->boolean('is_cancelled');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
